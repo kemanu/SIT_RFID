0f76fbd13b21397b69ededf2522a591e44c65cd5
kemanu <kyleemanuele@gmail.com>
2018-04-22 19:08:35 -0400

    Added SQLite3 Functionality, Cleaned up LEDS

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sqlite3 as lite
+import sys
 import SimpleMFRC522
 import RPi.GPIO as GPIO
 from flask import Flask, render_template
@@ -10,11 +12,41 @@ reader = SimpleMFRC522.SimpleMFRC522()
 
 @app.route("/")
 def index():
-	id,text = reader.read()
+	id,text = reader.read_no_block()
+	stevensID = 0
+	LEDStatus = "Space available"
+	if text != None:
+		text = text.strip()
+		LEDStatus = "Space available"
+	if text == "Stevens":
+		stevensID = 1
+		LEDStatus = "Space occupied"
+	elif text == None:
+		stevensID = 0
+	else:
+		stevensID = 2
+	with lite.connect('RFID.db') as con:
+		cur = con.cursor()
+		cur.execute("SELECT FirstName,LastName,Year FROM Users WHERE Id=?",(id,))
+		data = cur.fetchone()
+		if data == None:
+			stevensID = 2
+			firstName = "N/A"
+			lastName = "N/A"
+			year = "N/A"
+		else:
+			firstName = data[0]
+			lastName = data[1]
+			year = data[2]
 	templateData = {
 		'TITLE' : 'RFID Number',
 		'ID' : id,
 		'TEXT' : text,
+		'stevensID' : stevensID,
+		'STATUS' : LEDStatus,
+		'FIRSTNAME' : firstName,
+		'LASTNAME' : lastName,
+		'YEAR' : year,
 	}
 	return render_template('index.html', **templateData)
 if __name__ == "__main__":
